57776324ad93f651bdda643dfd2fb39202a13849
Тетяна Невідома <nevidomatanya89@gmail.com>
2023-03-20 20:10:10 +0200

    upd

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import {StatSection, StatTitle, StatList, StatItem, Label} from './Statistcs.styled';
+
+export default function Statistics({title, data}) {
+    return (
+        <StatSection>
+            {title && <StatTitle>{title}</StatTitle>}
+
+            <StatList>
+                {data.map((stat, index) => (
+                    <StatItem 
+                        i={index}
+                        key={stat.id}>
+                            <Label>{stat.label}</Label>
+                            <span>{stat.percentage}</span>
+                    </StatItem>
+                )
+                )}
+            </StatList>
+        </StatSection>
+    )
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired
+    }))
+}
